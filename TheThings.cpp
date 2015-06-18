7365a7d4b60fbab0492ab39a94ed80351256c93d
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2015-06-18 16:03:53 +0200

    Add library

---
+++ TheThings.cpp
@@ -0,0 +1,119 @@
+#include "TheThings.h"
+
+#define TOKEN_SIZE 43
+
+byte TheThings::server[] = {77, 73, 82, 243};
+
+TheThings::TheThings() {
+    this->token = "";
+    this->data = "";
+}
+
+TheThings::TheThings(String token) {
+    this->token = token;
+    this->data = "";
+}
+
+String TheThings::activate(String activationCode) {
+    token = "";
+    String body = "";
+    String find_token = "thingToken\":\"";
+    int start;
+    char c;
+    if (POST(client, "/v2/things/", "{ \"activationCode\":\"" + activationCode + "\"}")) {
+        delay(1000);
+        while (client.available() && (c = (char)client.read()) != '{');
+        if (c == '{') {
+            while (client.available() && (c = (char)client.read()) != '\n') body.concat(c);
+            if ((start = body.indexOf(find_token)) >= 0) {
+                start = start + find_token.length();
+                token = body.substring(start, start + TOKEN_SIZE);
+            }
+        }
+    }
+    return token;
+}
+
+String TheThings::getToken() {
+    return token;
+}
+
+void TheThings::addValue(String key, String value) {
+    data.concat("{\"key\":\"" + key + "\",\"value\":\"" + value + "\"}");
+}
+
+void TheThings::addValue(String key, double value) {
+    addValue(key, String(value));
+}
+
+String TheThings::send() {
+    data = "";
+    String received = "";
+    if (POST(client, "/v2/things/" + token, "{\"values\":[" + data + "]}")) {
+        delay(1000);
+        while (client.available()) {
+            char c = (char)client.read();
+            received.concat(c);
+        }
+    }
+    return received;
+}
+
+bool TheThings::POST(EthernetClient &client, String url, String &data) {
+    client.flush();
+    if (client.connected() || client.connect(server, 80)) {
+        client.print("POST " + url + " HTTP/1.1\n");
+        client.print("Host: api.thethings.io\n");
+        client.print("Connection: close\n");
+        client.print("Content-Type: application/json\n");
+        client.print("Content-Length: ");
+        client.print(data.length());
+        client.print("\n\n");
+        client.print(data);
+    }
+    return client.connected();
+}
+
+bool TheThings::GET(EthernetClient &client, String &data) {
+    client.flush();
+    if (client.connected() || client.connect(server, 80)) {
+        client.print("GET " + data + " HTTP/1.1\n");
+        client.print("Host: api.thethings.io\n");
+        client.print("Accept: application/json\n\n");
+    }
+    return client.connected();
+}
+
+String TheThings::read(String key) {
+    return read(key, 10);
+}
+
+String TheThings::read(String key, int limit) {
+    String received = "";
+    if (client.connect(server, 80)) {
+        GET(client, "/v2/things/" + token + "/resources/" + key + "?limit=" + String(limit));
+        delay(1000);
+        while (client.available())
+            received.concat((char)client.read());
+        client.stop();
+    }
+    return received;
+}
+
+void TheThings::subscribe() {
+    subscription_client.stop();
+    GET(subscription_client, "/v2/things/" + token);
+}
+
+bool TheThings::available() {
+    return subscription_client.available();
+}
+
+String TheThings::read() {
+    String received = "";
+    if (subscription_client.connected()) {
+        while (subscription_client.available())
+            received.concat((char)subscription_client.read());
+    }
+    return received;
+}
---
+++ TheThings.h
@@ -0,0 +1,65 @@
+// Copyright (c) 2015 TheThings.iO
+// This file is licensed under the MIT License
+// http://opensource.org/licenses/MIT
+
+#ifndef THETHINGS_H
+#define THETHINGS_H
+
+#include "Arduino.h"
+#include "Ethernet.h"
+
+class TheThings {
+public:
+    TheThings();
+    TheThings(String);
+
+    // Create a new thing passing it's existing token as an argument or leave
+    // it empty to activate it later using "activate" function.
+    String activate(String);
+
+    // Return thing's token
+    String getToken();
+
+    // To write a variable into the theThings.iO cloud, call this function with
+    // the value to write. This function can be called any times to add more
+    // variables to be sent. Finally, call the "send" function to actually
+    // write the values.
+
+    // key: string
+    // value: string
+    void addValue(String, String);
+
+    // key: string
+    // value: number
+    void addValue(String, double);
+
+    // Actually send the values to theThings.iO. See function "addValue".
+    String send();
+
+    // Read a variable from the theThings.iO. If only the argument "key" is
+    // specified, the last value will be returned. This function will return
+    // "limit" number of values of the variable inside an array.
+    //
+    // key: name of the variable
+    // limit: max number of values to return.
+    // startDate and endDate format: YYYYMMDDHHmmss
+    String read(String);
+    String read(String, int);
+
+    void subscribe();
+    bool available();
+    String read();
+
+private:
+    String token;
+    String data;
+    EthernetClient client;
+    EthernetClient subscription_client;
+    static byte server[];
+
+    // Private functions
+    bool POST(EthernetClient &, String, String &);
+    bool GET(EthernetClient &, String &);
+};
+
+#endif // THETHINGS_H
